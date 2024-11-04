8136d6a82866d0d41e4fec87ac230d75ed8f01f4
Júlio António <julioantonio1408@gmail.com>
2024-11-04 07:49:52 +0100

    Novo commit inicial

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,25 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import {z} from "zod";
+
+const f = createUploadthing({
+  errorFormatter: (err) => {
+    return {
+      message: err.message,
+      zodError: err.cause instanceof z.ZodError ? err.cause.flatten() : null,
+    };
+  },
+});
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  .input(z.object({configId: z.string().optional()}))
+    .middleware(async ({ input }) => {
+      return {input}
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+        const {configId} = metadata.input
+      return { configId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
