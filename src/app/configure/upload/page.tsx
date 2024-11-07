25a595d7b85d5dc2f8291728f4e7057a11bcd6ca
Júlio António <julioantonio1408@gmail.com>
2024-11-07 19:01:39 +0100

    pagina de upload terminada...install prisma

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -42,7 +42,12 @@ const Page = () => {
 
 
     return (
-        <div className={cn("relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center", { "ring-blue-900/25 bg-blue-900/10": isDragOver })}>
+        <div className={cn(
+            'relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center',
+            {
+                'ring-blue-900/25 bg-blue-900/10': isDragOver,
+            }
+        )}>
             <div className="relative flex flex-1 flex-col items-center justify-center w-full">
                 <Dropzone onDropRejected={onDropRejected} onDropAccepted={onDropAccepted}
                     accept={{
